a6e97181f5105a34718eb4af79db5a3b950ce7ac
Graydon von Gossler <graydonvg@gmail.com>
2023-02-18 10:47:38 +0200

    reduced repeated code for random color function

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ const color2 = document.querySelector('.color2');
 const body = document.getElementById('gradient');
 const color1Default = document.querySelector('.color1').defaultValue;
 const color2Default = document.querySelector('.color2').defaultValue;
+const randomBtn = document.getElementById('random');
 
 // Default background on load
 function backgroundDefualt() {
   body.style.background = `linear-gradient(to right, ${color1Default}, ${color2Default})`;
 }
-
 document.onload = backgroundDefualt();
 
 // Display the initial CSS linear gradient property on page load
 function DisplayInitialCSS() {
   css.textContent = `${body.style.background};`;
 }
-
 document.onload = DisplayInitialCSS();
 
 // Custom background generator
@@ -26,7 +25,6 @@ function background() {
   // Display the CSS linear gradient property selected
   css.textContent = `${body.style.background};`;
 }
-
 color1.addEventListener('input', background);
 color2.addEventListener('input', background);
 
@@ -42,23 +40,18 @@ function randomBackground(random1, random2) {
   css.textContent = `${body.style.background};`;
 }
 
-const randomBtn = document.getElementById('random');
-const arrayOfColorFunctions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-
-function randomColor() {
-  let randomColorString2 = '#';
-  let randomColorString1 = '#';
+function randomHexValue(randomColorString) {
+  const arrayOfColorFunctions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
   for (let x = 0; x < 6; x += 1) {
     const index = Math.floor(Math.random() * 16);
     const value = arrayOfColorFunctions[index];
-    randomColorString1 += value;
+    randomColorString += value;
   }
-  for (let x = 0; x < 6; x += 1) {
-    const index = Math.floor(Math.random() * 16);
-    const value = arrayOfColorFunctions[index];
-    randomColorString2 += value;
-  }
-  randomBackground(randomColorString1, randomColorString2);
+  return randomColorString;
 }
 
+function randomColor() {
+  let randomColorString = '#';
+  randomBackground(randomHexValue(randomColorString), randomHexValue(randomColorString));
+}
 randomBtn.addEventListener('click', randomColor);
